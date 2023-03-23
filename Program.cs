6e5c2e8997a297cb1b0729c383f46601def5712e
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-23 12:30:34 +0100

    ok

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+var client = new RestClient("http://adressen");
+            var request = new RestRequest("/", Method.Get);
+            RestResponse response = client.Execute(request);
+            String content = response.Content;
+Root menu = JsonConvert.DeserializeObject<Root>(content); // Byt ut till er eget huvudobjekt
+public class Geometry
+    {
+        public string type { get; set; }
+        public List<List<double>> coordinates { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string name { get; set; }
+        public string levelType { get; set; }
+        public int level { get; set; }
+        public string unit { get; set; }
+        public List<double> values { get; set; }
+    }
+
+    public class Root
+    {
+        public DateTime approvedTime { get; set; }
+        public DateTime referenceTime { get; set; }
+        public Geometry geometry { get; set; }
+        public List<TimeSeries> timeSeries { get; set; }
+    }
+
+    public class TimeSeries
+    {
+        public DateTime validTime { get; set; }
+        public List<Parameter> parameters { get; set; }
+    }
+
