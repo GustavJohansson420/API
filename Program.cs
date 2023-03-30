7c74fecece93e76df788abe12c31a6bc0aa5cabb
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-30 12:30:53 +0200

    random activety

--- Program.cs
+++ Program.cs
@@ -1,37 +1,22 @@
 ﻿using Newtonsoft.Json;
 using RestSharp;
 
-var client = new RestClient("http://adressen");
+var client = new RestClient("https://www.boredapi.com/api/activity");
             var request = new RestRequest("/", Method.Get);
             RestResponse response = client.Execute(request);
             String content = response.Content;
 Root menu = JsonConvert.DeserializeObject<Root>(content); // Byt ut till er eget huvudobjekt
-public class Geometry
-    {
-        public string type { get; set; }
-        public List<List<double>> coordinates { get; set; }
-    }
 
-    public class Parameter
-    {
-        public string name { get; set; }
-        public string levelType { get; set; }
-        public int level { get; set; }
-        public string unit { get; set; }
-        public List<double> values { get; set; }
-    }
+Console.WriteLine(menu.activity);
 
     public class Root
     {
-        public DateTime approvedTime { get; set; }
-        public DateTime referenceTime { get; set; }
-        public Geometry geometry { get; set; }
-        public List<TimeSeries> timeSeries { get; set; }
-    }
-
-    public class TimeSeries
-    {
-        public DateTime validTime { get; set; }
-        public List<Parameter> parameters { get; set; }
+        public string activity { get; set; }
+        public string type { get; set; }
+        public int participants { get; set; }
+        public float price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public double accessibility { get; set; }
     }
 
